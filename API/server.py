d09fb6218d5aac2d91722c28c48ad002027a4a5d
Angnami <mhtgoudjemi@gmail.com>
2022-12-08 14:20:07 +0100

    Add testing files

--- API/server.py
+++ API/server.py
@@ -3,7 +3,6 @@ import os, io
 from efficientnet import EfficientNetModel
 from PIL import Image
 from fastapi import FastAPI, Request, File, UploadFile
-import fastapi
 
 # Loading the model from efficientnet.py
 model = EfficientNetModel('efficientnet_full_model.h5')
@@ -22,7 +21,7 @@ def welcome():
     return {'message':'Welcome to the pyfungivisum API!'}
 
 # Creating a route to allow to client to send request
-@app.post('/predict',name='Specy Prediction')
+@app.post('/predict',name='Prediction')
 def predict_specy(request:Request,file:UploadFile = File(...)):
     content = file.file.read()
     image = Image.open(io.BytesIO(content)).convert('RGB')
