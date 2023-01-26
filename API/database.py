f01b4f93b5b5936112e4a3bf63085b8a7fb01836
Angnami <mhtgoudjemi@gmail.com>
2023-01-26 03:20:14 +0100

    Update database.py file

--- API/database.py
+++ API/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://devuser:2MYSQL&password@127.0.0.1:3306/pyfungivisum"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://username:password@127.0.0.1:3306/pyfungivisum"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
