2925e7efc074e87e7c9700ba605f4a873fd13cf6
Angnami <mhtgoudjemi@gmail.com>
2023-03-12 13:39:16 +0100

    modified database.py file

--- API/database/database.py
+++ API/database/database.py
@@ -2,10 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
-user_name = os.environ.get("MYSQL_USER")
-user_password = os.environ.get("MYSQL_PASSWORD")
-mysql_url = os.environ.get("MYSQL_URL")
-database_name = os.environ.get("MYSQL_DATABASE")
+
+if os.environ.get('ENV_TYPE') == "DEVELOPMENT":
+
+    user_name = os.environ.get("MYSQL_USER")
+    user_password = os.environ.get("MYSQL_PASSWORD")
+    mysql_host = os.environ.get("MYSQL_HOST")
+    mysql_port = int(os.environ.get('MYSQL_PORT'))
+    database_name = os.environ.get("MYSQL_DATABASE")
+else:
+    user_name = os.environ.get("MYSQL_USER")
+    user_password = os.environ.get("MYSQL_PASSWORD")
+    mysql_port = int(os.environ.get("MYSQL_PORT"))
+    mysql_host = os.environ.get("MYSQL-SERVICE-HOST")
+    database_name = os.environ.get("MYSQL_DATABASE")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user_name}:{user_password}@{mysql_url}:3306/{database_name}"
 
